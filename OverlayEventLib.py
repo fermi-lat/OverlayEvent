ce29641f2d28fbbef98204ff56bad038e2daf9f2
heather <>
2011-07-27 14:42:05 +0000

    really adding in SCons files

--- OverlayEventLib.py
+++ OverlayEventLib.py
@@ -1,4 +1,4 @@
-#$Header:  $
+#$Header: /nfs/slac/g/glast/ground/cvs/OverlayEvent/OverlayEventLib.py,v 1.1 2009/08/06 01:50:49 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['OverlayEvent'],
