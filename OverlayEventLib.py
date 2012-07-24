70fe123334e9602681b42d4ad144e2825d252552
jrb <>
2012-07-24 20:33:36 +0000

    add dependence on Gaudi

--- OverlayEventLib.py
+++ OverlayEventLib.py
@@ -1,4 +1,4 @@
-#$Header:  $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/OverlayEvent/OverlayEventLib.py,v 1.1 2009/08/06 01:50:49 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['OverlayEvent'],
@@ -6,6 +6,7 @@ def generate(env, **kw):
     env.Tool('geometryLib')
     env.Tool('identsLib')
     env.Tool('addLibrary', library = env['clhepLibs'])
+    env.Tool('addLibrary', library = env['gaudiLibs'])
 
 def exists(env):
     return 1;
