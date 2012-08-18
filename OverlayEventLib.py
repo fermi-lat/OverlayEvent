ca5b352809665a06b9d8fb9094fa6e31ab9c1f9a
jrb <>
2012-08-18 00:33:14 +0000

    support no-install headers on Windows

--- OverlayEventLib.py
+++ OverlayEventLib.py
@@ -1,8 +1,13 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/OverlayEvent/OverlayEventLib.py,v 1.1 2009/08/06 01:50:49 jrb Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/OverlayEvent/OverlayEventLib.py,v 1.2 2012/07/24 20:33:36 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['OverlayEvent'],
                  package = 'OverlayEvent')
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'OverlayEvent') 
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'enums') 
+        env.Tool('findPkgPath', package = 'Event') 
     env.Tool('geometryLib')
     env.Tool('identsLib')
     env.Tool('addLibrary', library = env['clhepLibs'])
